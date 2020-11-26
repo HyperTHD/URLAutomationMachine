14b881425c8b29d24100c534074507f700cba259
Abdulbasid Guled <guled.basid@gmail.com>
2020-11-25 23:29:33 -0500

    Sorted out files, Added test cases, and requirements.txt to help install needed modules

--- src/url_class.py
+++ src/url_class.py
@@ -15,7 +15,8 @@ class urlAutomationMachine:
         self.listOfUrls = []
         self.isJson = isJson
         self.http = urllib3.PoolManager()
-        self.ignore_list = None
+        self.response_status = None
+        self.ignore_list = []
         if ignore is not None:
             ignore_file = open(ignore, "r")
 
@@ -24,24 +25,44 @@ class urlAutomationMachine:
             )
 
     def processUrl(self):
-        self.makeRequest(self.input)
+        if self.input is not None:
+            if type(self.input) == str:
+                try:
+                    self.checkUrl()
+                    self.makeRequest(self.input)
+                except:
+                    raise ValueError("Input was not a valid URL")
+        else:
+            raise AttributeError("Function requires a url that's a string")
 
     def processFile(self):
+        if self.input is not None:
+            if type(self.input) == str:
+                fileToCheck = open(self.input, "r")
+                self.listOfUrls = re.findall(r"https?:[a-zA-Z0-9_.+-/#~]+", fileToCheck.read())
 
-        fileToCheck = open(self.input, "r")
-        self.listOfUrls = re.findall(r"https?:[a-zA-Z0-9_.+-/#~]+", fileToCheck.read())
+                if self.listOfUrls == []:
+                    pass
 
-        for line in self.listOfUrls:
-            line = line.strip()
-            if line not in self.ignore_list:
-                self.makeRequest(line)
+                for line in self.listOfUrls:
+                    line = line.strip()
+                    if line not in self.ignore_list:
+                        self.makeRequest(line)
+
+                fileToCheck.close()
+            else:
+                raise ValueError("Function requires a file to be inserted")
+        else:
+            raise AttributeError("A parameter is required")
 
     def makeRequest(self, url):
         init()
         try:
             r = self.http.request("HEAD", url)
+            self.response_status = r
             self.printOutput(r, url)
-        except urllib3.exceptions.MaxRetryError:  # At this point, the connection attempt timed out and therfore, the url cannot be reached, so in this case, we skip the url entirely.
+        except urllib3.exceptions.MaxRetryError as e:  # At this point, the connection attempt timed out and therfore, the url cannot be reached, so in this case, we skip the url entirely.
+            print("Url does not load fast enough")
             pass
 
     def printOutput(self, r, url):
@@ -79,6 +100,9 @@ class urlAutomationMachine:
                     + f"[UNKNOWN] {url} gives off a warning. This url is fishy!"
                 )
 
+    def getStatus(self) -> int:
+        return self.response_status.status
+
     def processTelescope(self):
         telescopeURL = "http://localhost:3000/posts"  # Local host telescope url to grab data locally
         try:
@@ -88,3 +112,11 @@ class urlAutomationMachine:
                 self.makeRequest(f"{telescopeURL}/{post['id']}")
         except urllib3.exceptions.MaxRetryError as e:  # At this point, the connection attempt timed out and therfore, the url cannot be reached, so in this case, we skip the url entirely.
             print(str(e))
+
+    def checkUrl(self) -> str or None:
+        valid_search = re.search(r"https?:[a-zA-Z0-9_.+-/#~]+", self.input)
+
+        if valid_search is not None:
+            return valid_search.string
+        else:
+            return None
--- url_checker.py
+++ url_checker.py
@@ -2,7 +2,7 @@
 import threading
 import sys
 import argparse
-from url_class import urlAutomationMachine
+from src.url_class import urlAutomationMachine
 
 # checkURL function parses a url and make a request to check whether the url is broken or not.
 # Displays output based on -j argument
@@ -11,13 +11,13 @@ from url_class import urlAutomationMachine
 
 
 def check_url(file_input, is_json=False):
-  """Creates a machine object to process a single URL"""
-  if is_json:
-    url_machine = urlAutomationMachine(file_input, is_json)
-    url_machine.processUrl()
-  else:
-    url_machine = urlAutomationMachine(file_input)
-    url_machine.processUrl()
+    """Creates a machine object to process a single URL"""
+    if is_json:
+        url_machine = urlAutomationMachine(file_input, is_json)
+        url_machine.processUrl()
+    else:
+        url_machine = urlAutomationMachine(file_input)
+        url_machine.processUrl()
 
 
 # checkFile function parses each line looking for urls to check and make requests
@@ -27,13 +27,13 @@ def check_url(file_input, is_json=False):
 
 
 def check_file(file_input, is_json=False, ignore=None):
-  """Creates a machine object to process a file of URLs"""
-  if is_json:
-    url_machine = urlAutomationMachine(file_input, is_json, ignore)
-    url_machine.processFile()
-  else:
-    url_machine = urlAutomationMachine(file_input, False, ignore)
-    url_machine.processFile()
+    """Creates a machine object to process a file of URLs"""
+    if is_json:
+        url_machine = urlAutomationMachine(file_input, is_json, ignore)
+        url_machine.processFile()
+    else:
+        url_machine = urlAutomationMachine(file_input, False, ignore)
+        url_machine.processFile()
 
 
 # checkTelescope function makes a request to "Telescope" to parse the 10 latest post urls
@@ -43,13 +43,13 @@ def check_file(file_input, is_json=False, ignore=None):
 
 
 def check_telescope(file_input=None, is_json=False):
-  """Creates a machine object to process the 10 latest telescope posts"""
-  if is_json:
-    url_machine = urlAutomationMachine(file_input, is_json)
-    url_machine.processTelescope()
-  else:
-    url_machine = urlAutomationMachine(file_input)
-    url_machine.processTelescope()
+    """Creates a machine object to process the 10 latest telescope posts"""
+    if is_json:
+        url_machine = urlAutomationMachine(file_input, is_json)
+        url_machine.processTelescope()
+    else:
+        url_machine = urlAutomationMachine(file_input)
+        url_machine.processTelescope()
 
 
 # Main function parses each line looking for urls to check and make requests
@@ -60,59 +60,61 @@ def check_telescope(file_input=None, is_json=False):
 
 
 def main(arguments):
-  """Main function creates a thread based on argument passed to main program"""
-  if arguments.f:
-    try:
-      threading.Thread(target=check_file(arguments.f, arguments.j, arguments.i)).start()
-    except Exception as error_exception:
-      sys.stderr.write(str(error_exception))
-      sys.exit(1)
-    sys.exit(0)
-  elif arguments.u:
-    try:
-      threading.Thread(target=check_url(arguments.u, arguments.j)).start()
-    except Exception as error_exception:
-      sys.stderr.write(str(error_exception))
-      sys.exit(1)
-    sys.exit(0)
-  elif arguments.t:
-    try:
-      threading.Thread(target=check_telescope(arguments.t, arguments.j)).start()
-    except Exception as error_exception:
-      sys.stderr.write(str(error_exception))
-      sys.exit(1)
-    sys.exit(0)
-  elif arguments.v:
-    print("URLAutomationMachine Ver 2.0")
-  else:
-    print(
-        """This program has two arguments, one for inputting the file,
+    """Main function creates a thread based on argument passed to main program"""
+    if arguments.f:
+        try:
+            threading.Thread(
+                target=check_file(arguments.f, arguments.j, arguments.i)
+            ).start()
+        except Exception as error_exception:
+            sys.stderr.write(str(error_exception))
+            sys.exit(1)
+        sys.exit(0)
+    elif arguments.u:
+        try:
+            threading.Thread(target=check_url(arguments.u, arguments.j)).start()
+        except Exception as error_exception:
+            sys.stderr.write(str(error_exception))
+            sys.exit(1)
+        sys.exit(0)
+    elif arguments.t:
+        try:
+            threading.Thread(target=check_telescope(arguments.t, arguments.j)).start()
+        except Exception as error_exception:
+            sys.stderr.write(str(error_exception))
+            sys.exit(1)
+        sys.exit(0)
+    elif arguments.v:
+        print("URLAutomationMachine Ver 2.0")
+    else:
+        print(
+            """This program has two arguments, one for inputting the file,
         the second one displays the current version of the program"""
-    )
-    print("Usage: urlChecker [-f] inputFile: The input file to be processed")
-    print("Usage: urlChecker [-v]: Displays current version of the program")
-    print("Usage: urlChecker [-u] inputUrl: Checks URL to see if it works or not")
-    print("Usage: urlChecker [-j]: Displays result in JSON format")
-    print("Usage: urlChecker [-i]: Ignores the URL that's passed as an argument")
+        )
+        print("Usage: urlChecker [-f] inputFile: The input file to be processed")
+        print("Usage: urlChecker [-v]: Displays current version of the program")
+        print("Usage: urlChecker [-u] inputUrl: Checks URL to see if it works or not")
+        print("Usage: urlChecker [-j]: Displays result in JSON format")
+        print("Usage: urlChecker [-i]: Ignores the URL that's passed as an argument")
 
 
 if __name__ == "__main__":
 
-  parse = argparse.ArgumentParser(
+    parse = argparse.ArgumentParser(
         description="Checks the file input for any broken HTML urls"
     )
-  parse.add_argument("-f", help="The input file to check")
-  parse.add_argument(
+    parse.add_argument("-f", help="The input file to check")
+    parse.add_argument(
         "-v", action="store_true", help="Displays current version of program"
     )
-  parse.add_argument("-u", help="The URL to check")
-  parse.add_argument("-j", action="store_true", help="Displays output in JSON format")
-  parse.add_argument("-i", help="Ignores any url that matches the argument")
-  parse.add_argument(
+    parse.add_argument("-u", help="The URL to check")
+    parse.add_argument("-j", action="store_true", help="Displays output in JSON format")
+    parse.add_argument("-i", help="Ignores any url that matches the argument")
+    parse.add_argument(
         "-t",
         action="store_true",
         help="Will ignore the file given and instead check the 10 latest posts to telescope",
     )
-  args = parse.parse_args()
+    args = parse.parse_args()
 
-  main(args)
+    main(args)
